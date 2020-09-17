28f25a5348f8421169a831eaa6d953cab24e59ae
Gopi Krishna M <gopt.muktevi@gmail.com>
2020-09-17 23:48:10 +0530

    Initial Commit and project setup

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+const Index = () => {
+    return (
+        <div>
+            <h2>Welcome to the world of Gopi Krishna M.</h2>
+        </div>
+    )
+};
+
+ReactDOM.render(
+    <Index />,
+    document.getElementById('app')
+)
